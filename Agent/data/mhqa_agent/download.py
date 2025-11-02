28f67f7d7b04a3af2064109830d44ae9991a7477
jiaqi <kris@chainopera.com>
2025-11-02 07:16:00 -0800

    upload datasets

--- Agent/data/mhqa_agent/download.py
+++ Agent/data/mhqa_agent/download.py
@@ -1,8 +1,8 @@
 from datasets import load_dataset
 
 # Login using e.g. `huggingface-cli login` to access this dataset
-ds_sft = load_dataset("PersonalAILab/AFM-MHQA-Agent-SFT-Dataset")
-ds_sft.save_to_disk("./AFM-MHQA-Agent-SFT-Dataset")
+ds_sft = load_dataset("Chtistina777/GAP-MHQA-SFT-7K")
+ds_sft.save_to_disk("./GAP-MHQA-SFT-Dataset")
 
-ds_rl = load_dataset("PersonalAILab/AFM-MHQA-RL-Dataset")
-ds_rl.save_to_disk("./AFM-MHQA-Agent-RL-Dataset")
+ds_rl = load_dataset("Chtistina777/GAP-MHQA-RL")
+ds_rl.save_to_disk("./GAP-MHQA-RL-Dataset")
